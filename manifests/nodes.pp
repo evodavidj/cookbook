b02e968e4c441986fd8f09c54da3e7e7bdc7af61
DavidJohnson <david.johnson@evopayments.com>
2014-06-17 13:07:59 -0700

    importing nodes.pp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,10 +1,7 @@
 # chptr 1 pg 17 - creating a papply command/script
 #
-<<<<<<< HEAD
-node 'cookbook', 'cookbook2' {
-=======
-node 'puppet' {
->>>>>>> b2da083000e2e9f58990a01e6f8f6b3405f2139f
+#<<<<<<< HEAD
+node 'cookbook2', 'puppet' {
  include puppet
 }
 
